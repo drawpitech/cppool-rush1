94a10666c0eb8c3df279c5f5ba706c8e93e6629a
Jules Fradin <jules.fradin86@gmail.com>
2024-01-05 22:34:32 +0100

    add: all operations

--- Utils.hs
+++ Utils.hs
@@ -10,4 +10,20 @@ sasb lx = lx
 sc :: [la] -> [lb] -> ([la], [lb])
 sc la lb = (sasb la, sasb lb)
 
+-- Take the first element of l1 and prepend it to l2
+papb :: [lx] -> [lx] -> ([lx], [lx])
+papb (lxHead : lxTail) (lyHead : lyTail) = (lxTail, lxHead : lyHead : lyTail)
+papb lx ly = (lx, ly)
 
+rarb :: [lx] -> [lx]
+rarb (lxHead : lxTail) = lxTail ++ [lxHead]
+
+rr :: [lx] -> [ly] -> ([lx], [ly])
+rr lx ly = (rarb lx, rarb ly)
+
+rrarrb :: [lx] -> [lx]
+rrarrb [] = []
+rrarrb lx = last lx : init lx
+
+rrr :: [lx] -> [lx] -> ([lx], [lx])
+rrr lx ly = (rrarrb lx, rrarrb ly)
