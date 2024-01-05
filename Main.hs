a54fcedea042522c73ab3b7044652a575af9d5a9
Clément <clement2104.boillot@gmail.com>
2024-01-05 21:36:04 +0100

    Add parsing for the arguments

--- Main.hs
+++ Main.hs
@@ -7,18 +7,29 @@
 
 module Main where
 
+import System.Environment (getArgs)
+import Utils (isSorted)
+
+getLa :: IO [Int]
+getLa = getArgs >>= return . map read
+
 getOpers :: IO [String]
 getOpers = getLine >>= return . words
 
+err :: [Int] -> [Int] -> IO ()
+err la lb = putStr "KO: " >> print (la, lb)
+
 exit :: [Int] -> [Int] -> IO ()
-exit _ [] = putStrLn "OK"
-exit la lb = putStr "KO: " >> print (la, lb)
+exit la [] | isSorted la = putStrLn "OK"
+           | otherwise = err la []
+exit la lb = err la lb
 
 execOper :: [Int] -> [Int] -> [String] -> IO ()
 execOper la lb [] = exit la lb
 execOper la lb (_:xs) = execOper la lb xs
 
 main :: IO ()
-main = getOpers >>= execOper la lb
-    where la = []
-          lb = []
+main = do op <- getOpers
+          la <- getLa
+          execOper la lb op
+       where lb = []
