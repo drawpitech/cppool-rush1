d8f858ad886fe1ec25f32a0b1baae1082497aa84
Clément <clement2104.boillot@gmail.com>
2024-01-05 21:18:39 +0100

    Add messages and skeleton for the project

--- Main.hs
+++ Main.hs
@@ -7,5 +7,18 @@
 
 module Main where
 
+getOpers :: IO [String]
+getOpers = getLine >>= return . words
+
+exit :: [Int] -> [Int] -> IO ()
+exit _ [] = putStrLn "OK"
+exit la lb = putStr "KO: " >> print (la, lb)
+
+execOper :: [Int] -> [Int] -> [String] -> IO ()
+execOper la lb [] = exit la lb
+execOper la lb (_:xs) = execOper la lb xs
+
 main :: IO ()
-main = putStrLn "OK"
+main = getOpers >>= execOper la lb
+    where la = []
+          lb = []
